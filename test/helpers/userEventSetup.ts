3b566b34e895bc34ad8c8d6471e6301f69a95b91
Jason O'Neill <CreativeTechGuy@users.noreply.github.com>
2023-11-05 02:38:12 -0800

    Simplify user-event types

--- test/helpers/userEventSetup.ts
+++ test/helpers/userEventSetup.ts
@@ -1,12 +1,11 @@
 import { jest } from "@jest/globals";
 import { userEvent } from "@testing-library/user-event";
+import type { Options, UserEvent } from "@testing-library/user-event";
 
 /**
  * Setup user-event instance with custom defaults
  */
-export function userEventSetup(
-    options: Parameters<(typeof userEvent)["setup"]>[0] = {}
-): ReturnType<(typeof userEvent)["setup"]> {
+export function userEventSetup(options: Options = {}): UserEvent {
     return userEvent.setup({
         advanceTimers: jest.advanceTimersByTime.bind(jest),
         ...options,
